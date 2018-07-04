3cef4fb26ef244dca952632be57be60b4ff53e5f
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2018-07-04 16:34:38 +0200

    Initial package settings

---
+++ setup.py
@@ -0,0 +1,25 @@
+import io
+
+from setuptools import setup
+from setuptools import find_packages
+
+from tomomibot import __version__
+
+with io.open('README.md', 'rt', encoding='utf8') as file:
+    readme = file.read()
+
+setup(name='tomomibot',
+      version=__version__,
+      description='Artificial intelligence bot for live voice improvisation',
+      long_description=readme,
+      author='Andreas Dzialocha',
+      author_email='kontakt@andreasdzialocha.com',
+      url='https://github.com/adzialocha/tomomibot',
+      license='MIT',
+      install_requires=[],
+      packages=find_packages(),
+      entry_points={
+            'console_scripts': {
+                'tomomibot = tomomibot.__main__:main',
+            }
+      })
