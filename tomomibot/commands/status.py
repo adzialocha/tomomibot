3c915e7d881bd268b3882df62bc6ea98cd3ce2e7
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2018-07-17 00:09:52 +0200

    Show voices in status command

--- tomomibot/commands/status.py
+++ tomomibot/commands/status.py
@@ -1,14 +1,16 @@
+import os
+
 import click
 
 from tomomibot.audio import all_inputs, all_outputs
 from tomomibot.cli import pass_context
-from tomomibot.utils import line
+from tomomibot.const import GENERATED_FOLDER
+from tomomibot.utils import line, check_valid_voice
 
 
 def list_audio_channels(ctx, channels):
     ctx.log(line())
-    ctx.log('{0:2} {1:8} {2:30}'.format(
-        ' #', 'Channels', 'Name'))
+    ctx.log('{0:2} {1:8} {2:30}'.format(' #', 'Channels', 'Name'))
     ctx.log(line())
     for i, chn in enumerate(channels):
         ctx.log('{0:2} {1:8} {2:30}'.format(
@@ -17,19 +19,39 @@ def list_audio_channels(ctx, channels):
             chn.name[:30]))
 
 
-def list_audio_devices(ctx):
-    inputs = all_inputs()
-    outputs = all_outputs()
-
-    ctx.log(click.style('Audio input devices', bold=True))
-    list_audio_channels(ctx, inputs)
-    print()
-    ctx.log(click.style('Audio output devices', bold=True))
-    list_audio_channels(ctx, outputs)
+def list_voices(ctx):
+    ctx.log(line())
+    ctx.log('{0:2} {1:30} {2:10}'.format(' #', 'Name', 'Status'))
+    ctx.log(line())
+    voice_dir = os.path.join(os.getcwd(), GENERATED_FOLDER)
+    for i, entry in enumerate(os.scandir(voice_dir)):
+        if entry.is_dir():
+            try:
+                check_valid_voice(entry.name)
+            except FileNotFoundError:
+                status = click.style('✘', fg='red')
+            else:
+                status = click.style('✓', fg='green')
+            ctx.log('{0:2} {1:30} {2:10}'.format(
+                i,
+                entry.name[:30],
+                status))
 
 
 @click.command('status', short_help='Display system info and audio devices')
 @pass_context
 def cli(ctx):
     """Display system info and audio devices."""
-    list_audio_devices(ctx)
+    inputs = all_inputs()
+    outputs = all_outputs()
+
+    ctx.log(click.style('Audio input devices', bold=True))
+    list_audio_channels(ctx, inputs)
+    ctx.log('')
+
+    ctx.log(click.style('Audio output devices', bold=True))
+    list_audio_channels(ctx, outputs)
+    ctx.log('')
+
+    ctx.log(click.style('Generated voices', bold=True))
+    list_voices(ctx)
