8f491d0419cb73adf418c94a3427f3aafb47174b
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-04-11 23:10:38 +0200

    Use python-osc for OSC message handling via OSC

--- tomomibot/server.py
+++ tomomibot/server.py
@@ -1,19 +1,57 @@
+import threading
+
+from pythonosc import dispatcher, osc_server
+
+from tomomibot.const import OSC_ADDRESS, OSC_PORT
+
+
 class Server:
 
     def __init__(self, ctx, **kwargs):
         self.ctx = ctx
         self.is_running = False
 
-        # @TODO read arguments for network setup
+        self.port = kwargs.get('port', OSC_PORT)
+        self.address = kwargs.get('address', OSC_ADDRESS)
+
+        # Prepare OSC message dispatcher and UDP server
+        disp = dispatcher.Dispatcher()
+        disp.map('/tomomibot/*', self._on_param)
+        bind = (self.address, self.port)
+        self._server = osc_server.ThreadingOSCUDPServer(bind, disp)
 
     def start(self):
+        thread = threading.Thread(target=self._start_server)
+        thread.daemon = True
+        thread.start()
+
         self.is_running = True
 
-        # @TODO start the http server
-
-        # @TODO start upd server
-
-        # @TODO start websocket server
-
     def stop(self):
+        self._server.shutdown()
         self.is_running = False
+
+    def _start_server(self):
+        self.ctx.log('OSC server @ {}:{}'.format(self.address,
+                                                 self.port))
+
+        self._server.serve_forever()
+
+    def _on_param(self, address, *args):
+        param = address.replace('/tomomibot/', '')
+
+        # Commands with no arguments
+        if param == 'reset':
+            print('Le Reset!')
+            return
+
+        # We expect one float argument from now on
+        if not len(args) == 1 or type(args[0]) is not float:
+            return
+
+        if param == 'volume':
+            print('Volume!', args[0])
+        elif param == 'temperature':
+            print('temperature', args[0])
+        elif param == 'interval':
+            print('interval', args[0])
